5e79feb4be44b3a099c4b0d31f5eec580908f763
Mahin <mahin610808@gmail.com>
2026-06-14 21:05:49 +0600

    final

---
+++ src/main/java/BasicProblem/palindromeNumber.java
@@ -0,0 +1,22 @@
+package BasicProblem;
+import java.util.Scanner;
+
+public class palindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int original=n;
+        int reverse=0;
+        while(n>0){
+            int digit=n % 10;
+            reverse =reverse *10+digit;
+            n=n/10;
+        }
+     if(original==reverse){
+         System.out.println("Palindrome:"+original);
+     }else{
+         System.out.println("Not Palindrome:"+reverse);
+     }
+
+    }
+}

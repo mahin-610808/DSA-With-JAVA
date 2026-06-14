17bb47e5defa13d179c67c9d6c807eafc0408287
Mahin <mahin610808@gmail.com>
2026-06-14 21:16:21 +0600

    final

---
+++ src/main/java/BasicProblem/CountDigit.java
@@ -0,0 +1,16 @@
+package BasicProblem;
+
+import java.util.Scanner;
+public class CountDigit {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter The Element:");
+        int n=sc.nextInt();
+        int count=0;
+        while (n>0){
+         n=n/10;
+         count++;
+        }
+        System.out.println(count);
+    }
+}
